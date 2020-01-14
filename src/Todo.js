6952f9957088eea38832fcea8c70eaa992c6da03
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-01-14 21:33:50 +0300

    Optimizing w/ Memo

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, memo } from 'react';
 import useToggleState from './hooks/useToggleState';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,7 +10,7 @@ import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import { DispatchContext } from './contexts/todos.context';
 
-export default function Todo({ id, task, completed }) {
+function Todo({ id, task, completed }) {
   const dispatch = useContext(DispatchContext);
   const [isEditing, toggle] = useToggleState(false);
   return (
@@ -45,3 +45,5 @@ export default function Todo({ id, task, completed }) {
     </ListItem>
   );
 }
+
+export default memo(Todo);
